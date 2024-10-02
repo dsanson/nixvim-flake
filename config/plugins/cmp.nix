b8c038e4c69ad66be8bf85ff4ace7ddf0fbd2e03
David Sanson <dsanson@gmail.com>
2024-10-01 19:02:06 -0500

    tab mapping for cmp

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -25,6 +25,34 @@
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({ select = false }),
+          ['<Tab>'] = function(fallback)
+            if not cmp.select_next_item() then
+              local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+              end
+              if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                cmp.complete()
+              else
+                fallback()
+              end
+            end
+          end,
+          ['<S-Tab>'] = function(fallback)
+            if not cmp.select_prev_item() then
+              local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+              end
+              if vim.bo.buftype ~= 'prompt' and has_words_before() then
+                cmp.complete()
+              else
+                fallback()
+              end
+            end
+          end,
         })
       '';
     };
