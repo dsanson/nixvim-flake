02f70ac9c5170f209b9782e4e9261db599f7d25d
David Sanson <dsanson@gmail.com>
2025-04-12 17:57:51 -0500

    todo shortcuts

--- config/plugins/todo-comments.nix
+++ config/plugins/todo-comments.nix
@@ -2,5 +2,15 @@
   plugins.todo-comments = {
     enable = true;
   };
+  extraConfigLua = ''
+    vim.keymap.set("n", "]t", function()
+      require("todo-comments").jump_next()
+    end, { desc = "Next todo comment" })
+
+    vim.keymap.set("n", "[t", function()
+      require("todo-comments").jump_prev()
+    end, { desc = "Previous todo comment" })
+  ''; 
 }
+
 
