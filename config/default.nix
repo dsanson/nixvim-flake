4cc28d6f65bacb54b8287e68938625800f858f2f
David Sanson <dsanson@gmail.com>
2025-04-15 23:17:05 -0500

    updating neopywal

--- config/default.nix
+++ config/default.nix
@@ -7,33 +7,38 @@
     ./digraphs.nix
     ./mappings.nix
 
-    ./plugins/auto-save.nix
-    ./plugins/cmp.nix
-    ./plugins/commentary.nix
     ./plugins/treesitter.nix
+    ./plugins/cmp.nix
+    ./plugins/lsp.nix
     ./plugins/which-key.nix
-    ./plugins/twilight.nix
+    ./plugins/trouble.nix
+
+    ./plugins/auto-save.nix
     ./plugins/lastplace.nix
+    ./plugins/oil.nix
+    ./plugins/rainbow-delimiters.nix
+    ./plugins/otter.nix
+    ./plugins/todo-comments.nix
+    ./plugins/neo-tree.nix
+    ./plugins/csv.nix
+    ./plugins/markdown-oxide.nix
+    ./plugins/pandoc.nix
+    ./plugins/render-markdown.nix
+    ./plugins/quarto.nix
+    
+    ./plugins/mini.nix
+    # replace with mini?
+    ./plugins/surround.nix
+    ./plugins/easy-align.nix
+    ./plugins/commentary.nix
+
+    # replace with snacks?
+    ./plugins/zen-mode.nix
+    ./plugins/twilight.nix
+    ./plugins/image.nix
     ./plugins/git-conflict.nix
     ./plugins/gitsigns.nix
-    ./plugins/surround.nix
-    ./plugins/rainbow-delimiters.nix
-    ./plugins/zen-mode.nix
-    ./plugins/otter.nix
     ./plugins/telescope.nix
-    ./plugins/todo-comments.nix
-    ./plugins/trouble.nix
-    ./plugins/neo-tree.nix
-    ./plugins/lsp.nix
-    ./plugins/easy-align.nix
-    ./plugins/markdown-oxide.nix
-    ./plugins/csv.nix
-    ./plugins/pandoc.nix
-    ./plugins/oil.nix
-    ./plugins/mini.nix
-    ./plugins/render-markdown.nix
-    ./plugins/image.nix
-    ./plugins/quarto.nix
 
   ];
 
@@ -48,8 +53,8 @@
      src = pkgs.fetchFromGitHub {
        owner = "RedsXDD";
        repo = "neopywal.nvim";
-       rev = "2d65d138463bbbe9d824b9bc1048ef1888017da6";
-       hash = "sha256-P28gg5fPPPyhepPHOZFgMnGTToCXK+v4ev6oTRSZXtg=";
+       rev = "09188d79b45694141ec779d05cbcc75f994639d1";
+       hash = "sha256-RLwxyGRmU1B8r6xO1YObF8qlNEj7qitNUArUlw092V8=";
      };
      doCheck = false;  
     })
