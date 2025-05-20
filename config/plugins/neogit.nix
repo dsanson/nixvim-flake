aebf4a28e58b8c51ccc888bb9795f6c699792cf7
David Sanson <dsanson@gmail.com>
2025-05-19 23:43:04 -0500

    Adding key maps for neogit

--- config/plugins/neogit.nix
+++ config/plugins/neogit.nix
@@ -1,5 +1,16 @@
 {
   plugins.neogit = {
     enable = true;
+    settings = {
+      graph_style = "unicode";
+      integrations = {
+        fzf-lua = false;
+        telecope = true;
+      };
+    };
   };
+  keymaps = [
+    { key = "<leader>gc"; action = "<cmd>NeogitCommit<cr>"; mode = ["n"]; options.desc = "git commit"; }
+    { key = "<leader>gg"; action = "<cmd>Neogit<cr>"; mode = ["n"]; options.desc = "Neogit"; }
+  ];
 }
