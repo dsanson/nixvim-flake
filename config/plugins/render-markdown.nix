536586937076745244c5ac51081d0fbaf22e8c1b
David Sanson <dsanson@gmail.com>
2026-03-08 20:41:26 -0500

    tweaking render-markdown settings

--- config/plugins/render-markdown.nix
+++ config/plugins/render-markdown.nix
@@ -1,26 +1,39 @@
 {
   plugins.render-markdown = {
     enable = true;
+    settings = {
+      file_types = [ "markdown" "pandoc" "carnap" ];
+      render_modes = true;
+      anti_conceal = {
+        above = 2;
+        below = 2;
+      };
+      bullet.icons = [ "⁃" "•" "⁃" "•" ];
+      html.comment.conceal = false;
+      checkbox.checked.icon = "✅ ";
+      checkbox.unchecked.icon = "⬜ ";
+      latex.enabled = false;
+    };
   };
 
-  extraConfigLua = ''
-    require('render-markdown').setup({
-      file_types = { 'markdown', 'pandoc', 'carnap' },
-      render_modes = true,
-      anti_conceal = {
-        above = 2,
-        below = 2,
-      },
-      bullet = {
-      icons = { '⁃', '•', '⁃', '•' },
-      },
-      html = {
-        comment = {
-          conceal = false,
-        },
-      },
-    })
-  '';
+  # extraConfigLua = ''
+  #   require('render-markdown').setup({
+  #     file_types = { 'markdown', 'pandoc', 'carnap' },
+  #     render_modes = true,
+  #     anti_conceal = {
+  #       above = 2,
+  #       below = 2,
+  #     },
+  #     bullet = {
+  #     icons = { '⁃', '•', '⁃', '•' },
+  #     },
+  #     html = {
+  #       comment = {
+  #         conceal = false,
+  #       },
+  #     },
+  #   })
+  # '';
 }
 
 
