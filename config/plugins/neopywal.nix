68785071375b15dcccf74adb2a05acfd969bb407
David Sanson <dsanson@gmail.com>
2026-06-07 12:08:24 -0500

    updating neopywal; adding option to silence notifications

--- config/plugins/neopywal.nix
+++ config/plugins/neopywal.nix
@@ -4,7 +4,7 @@
     (pkgs.vimUtils.buildVimPlugin {
      name = "neopywal";
      src = pkgs.fetchFromGitHub {
-       owner = "dsanson";
+       owner = "RedsXDD";
        repo = "neopywal.nvim";
        rev = "95425eea6411eebc17c2fcdc46d1c45e57d99205";
        hash = "sha256-Ytkt4WngDA7zlTUomr/BcyW+B6uULWE9OHm5aQE+mOM=";
@@ -15,5 +15,11 @@
 
   opts.background = "dark"; # neopywal manages light and dark on its own
   colorscheme = "neopywal";
+
+  extraConfigLua = ''
+     require('neopywal').setup({
+       notify = "warn";
+     })
+  '';
 
 }
